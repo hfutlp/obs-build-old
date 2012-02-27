5f12c8f507259c91375d479e853054b15b7903d6
Christian Schneemann <schneemann@b1-systems.de>
2012-02-27 20:10:45 +0100

    changed sysrotos into sysroot

--- Build.pm
+++ Build.pm
@@ -287,7 +287,7 @@ sub read_config {
     } elsif ($l0 eq 'hostarch:') {
       $config->{'hostarch'} = join(' ', @l);
     } elsif ($l0 eq 'sysroot:') {
-      push @{$config->{'sysroots'}}, { 'label' => $l[0], 'project' => $l[1], 'repository' => $l[2], 'arch' => $l[3], 'path' => $l[4], };
+      push @{$config->{'sysroot'}}, { 'label' => $l[0], 'project' => $l[1], 'repository' => $l[2], 'arch' => $l[3], 'path' => $l[4], };
     } elsif ($l0 eq 'crossbuild:') {
       $config->{'crossbuild'} = $l[0];
     } elsif ($l0 eq 'hostsysroot:') {
@@ -489,7 +489,7 @@ sub get_crossbuild {
 
 sub get_sysroots {
   my ($config) = @_;
-  return @{$config->{'sysroots'}};
+  return @{$config->{'sysroot'}};
 }
 
 sub get_hostsysroot {
